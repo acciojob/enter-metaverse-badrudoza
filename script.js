36033f94116e6d024f1df05da9fa139927f526dc
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-08 03:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let status=document.getElementById("status");
+let btn=document.getElementById("enterBtn");
+btn.addEventListener("click",()=>{
+	status.outerHTML='<h1 id="status">Enter the Metaverse</h1>';
+});
