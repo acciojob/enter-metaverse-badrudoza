d815aa7cad9760ee2ad6e8a8d9cd951289c4dd7e
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-08 03:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//your JS code here. If required.
-let status=document.getElementById("status");
-let btn=document.getElementById("enterBtn");
-btn.addEventListener("click",()=>{
-	status.outerHTML='<h1 id="status">Entered Metaverse</h1>';
+let status = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click", () => {
+  status.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
 });
